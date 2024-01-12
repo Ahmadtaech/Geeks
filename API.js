567b0723672d711d38234bc0cc0ca2e85820ea09
Ahmadtaech <ahmadshkh200007@gmail.com>
2024-01-12 14:01:36 +0200

    fox code

---
+++ API.js
@@ -0,0 +1,158 @@
+let adminName = 'AhDev'
+export const articlesArray = [
+  {},
+  {
+    poster: adminName,
+    articleName: 'HTML Inderction (lesson 1)',
+    date: '7 / 10 / 2023 ',
+    article: ' Welcome into HTML introduction <br> In the beginning HTML is a markup language used to build the structure of  websites pages and this language is the farst and main step in your  programming learning trip . <br>  HTML founded by Tim Berners-Lee in 1993 and this language has passed in several stages the first of them is HTML 1.0 then HTML 2.0 , HTML 3.0 , HTML 4.0 and last version is HTML 5.0 . <br> This language is very easy and you can use it with more languages as : CSS , JS , Python , PHP ... etc . And we will study this language in general and I will explain some of main tags which helps you to build websites pages easily . <br> And when you finish learning this language you can learn CSS in our website <a href="../css/lesson1.html">by this link</a> .<br><br> And now we will start learning the HTML . First this language depends on your understanding of this information: this language has many tags as <br> &lt;html&gt; &lt;body&gt; &lt;head&gt; ... and very more tags used in different sites . <br> And this image explain the tag structure <br> <img src="https://tutorial.techaltum.com/images/element.png" alt="HTML TAG" height="130px" style="  border-radius: 10px;filter: invert(100%)"> <br> Now you have this table that contains HTML tags . <br> <br> <h5 class="headers">HTML 5 Tags & Usage</h5> <br> <iframe style="width:100%;height:1000px;" src="./html tags .html" frameborder="0"></iframe> <br> By this table you can create any page structure fast <br> This lesson has finished. You can continue the second lesson using this link <a href="./lesson2.html">👉 Lesson 2 👈</a>',
+    shortArticle: 'Welcome into HTML introduction ... In the beginning HTML is a markup language used to build the structure of  websites pages and this language is the farst and main step in your  programming learning trip .',
+    learnMoreLink: 'pages/html/lesson1.html',
+    category: 'programming',
+    posterLogo: 'Images/Dev.png',
+    langName: 'HTML',
+    lessonNumer: 'leeson 1'
+  },
+  {
+    poster: adminName,
+    articleName: 'HTML Lesson 2',
+    date: '22 / 10 / 2023 ',
+    article: 'In the previous article we learn the basics of HTML , but in this lesson we will be up to a higher degree because we will learn how to think of creating sites before we begin with actual programming . <br> And this point is very important because the most of beginners in this field face difficulties in this idea because they are not ability to imagine the final form of the project . So , we will learn how to think... , but this skill develops over time and increased experience . <br> <br> First when you wont to build a website you should to think about the benefits of this poject , then you can start thinking about the form and design of this site . And I advise you to inspire of some similar sites that has the same content you would to make about . <br> As example when you wont to build a personal website to apply on a job , here you see the closest appropriate examples for your choice and collect ideas and designs you like , but you should not try the same design you liked accurately ; because you can add your own thouches maked the traditional design vibrant and custom for you and you will feel frustrated because you were not creative , so you have to think allot about the project before start coding . <br> <br> This lesson has finished. You can continue the next lesson using this link <a href="./lesson3.html">👉 Lesson 3 👈</a>',
+    shortArticle: 'In the previous article we learn the basics of HTML , but in this lesson we will be up to a higher degree because we will learn how to think of creating sites before we begin with actual programming .',
+    learnMoreLink: 'pages/html/lesson2.html',
+    category: 'programming',
+    posterLogo: 'Images/Dev.png',
+    langName: 'HTML',
+    lessonNumer: 'leeson 2'
+  },
+  {
+    poster: adminName,
+    articleName: 'HTML Project (Lesson 3)',
+    date: '17 / 12 / 2023 ',
+    article: 'Welcome to Lesson 3 in HTML definition. <br> In this group of articles we explaine HTML from zero and now we reached the last lesson in this group. In this lesson we will explain how to build small project to application your information which you learned in the previous lessons. Our project now is small portfolio website you can edit on it and make it more perfect. Here I will explain the HTML structure for this site but it’s need CSS and JavaScript files and I will explain the CSS and JavaScript codes in theres lessons. And I will put the download link for this files (Just HTML) . If you wont to download the full project’s file you can download it from <a href="../js/lesson3.html">the last JavaScript lesson </a>. In this paragraph I will explain all codes and tags which I used in this site .<div class="note"><h1>Note</h1><hr>This site stylish build to the mobile screen and in the CSS articles group we will learn how to make it responsive for all devices.</div>',
+    shortArticle: 'Welcome to Lesson 3 in HTML definition. <br> In this group of articles we explaine HTML from zero and now we reached the last lesson in this group.',
+    learnMoreLink: 'pages/html/lesson3.html',
+    category: 'programming',
+    posterLogo: 'Images/Dev.png',
+    langName: 'HTML',
+    lessonNumer: 'leeson 3'
+  },
+  {
+    poster: adminName,
+    articleName: 'CSS Inderction (lesson 1)',
+    date: '23 / 10 / 2023 ',
+    article: 'blog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articles',
+    shortArticle: 'blog specialist on technology and programming articles',
+    learnMoreLink: 'pages/css/lesson1.html',
+    category: 'programming',
+    posterLogo: 'Images/Dev.png',
+    langName: 'CSS',
+    lessonNumer: 'leeson 1'
+  },
+  {
+    poster: adminName,
+    articleName: 'CSS Lesson 2',
+    date: '23 / 10 / 2023 ',
+    article: 'blog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articles',
+    shortArticle: 'blog specialist on technology and programming articles',
+    learnMoreLink: 'pages/css/lesson1.html',
+    category: 'programming',
+    posterLogo: 'Images/Dev.png',
+    langName: 'CSS',
+    lessonNumer: 'leeson 2'
+  },
+
+
+  {
+    poster: adminName,
+    articleName: 'CSS Project (Lesson 3)',
+    date: '23 / 10 / 2023 ',
+    article: 'blog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articles',
+    shortArticle: 'blog specialist on technology and programming articles',
+    learnMoreLink: 'pages/css/lesson1.html',
+    category: 'programming',
+    posterLogo: 'Images/Dev.png',
+    langName: 'CSS',
+    lessonNumer: 'leeson 3'
+  },
+  {
+    poster: 'Rasheed Al-koas',
+    articleName: 'C++ lesson2',
+    date: '6 / 10 / 2023 ',
+    article: 'blog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articles',
+    shortArticle: 'blog specialist on technology and programming articles',
+    learnMoreLink: 'pages/html/lesson2',
+    category: 'programming',
+    posterLogo: 'Images/Dev.png'
+  },
+  {
+    poster: 'Khalid Alek',
+    articleName: 'Redmi note 12',
+    date: '6 / 10 / 2023 ',
+    article: 'blog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articles',
+    shortArticle: 'blog specialist on technology and programming articles',
+    learnMoreLink: 'pages/html/lesson2',
+    category: 'mobiles review',
+    posterLogo: 'Images/Dev.png'
+  },
+  {
+    poster: 'Khalid Alek',
+    articleName: 'Redmi note 12',
+    date: '6 / 10 / 2023 ',
+    article: 'blog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articles',
+    shortArticle: 'blog specialist on technology and programming articles',
+    learnMoreLink: 'pages/html/lesson2',
+    category: 'mobiles review',
+    posterLogo: 'Images/Dev.png'
+  },
+  {
+    poster: 'Khalid Alek',
+    articleName: 'Redmi note 12',
+    date: '6 / 10 / 2023 ',
+    article: 'blog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articles',
+    shortArticle: 'blog specialist on technology and programming articles',
+    learnMoreLink: 'pages/html/lesson2',
+    category: 'mobiles review',
+    posterLogo: 'Images/Dev.png'
+  },
+  {
+    poster: adminName,
+    articleName: 'How to install VScode on Android',
+    date: '6 / 10 / 2023 ',
+    article: 'blog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articles',
+    shortArticle: 'blog specialist on technology and programming articles',
+    learnMoreLink: 'pages/html/lesson2',
+    category: 'technology',
+    posterLogo: 'Images/Dev.png'
+  },
+  {
+    poster: adminName,
+    articleName: 'How to install VScode on Android',
+    date: '6 / 10 / 2023 ',
+    article: 'blog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articles',
+    shortArticle: 'blog specialist on technology and programming articles',
+    learnMoreLink: 'pages/html/lesson2',
+    category: 'technology',
+    posterLogo: 'Images/Dev.png'
+  },
+  {
+    poster: adminName,
+    articleName: 'How to install VScode on Android',
+    date: '6 / 10 / 2023 ',
+    article: 'blog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articles',
+    shortArticle: 'blog specialist on technology and programming articles',
+    learnMoreLink: 'pages/html/lesson2',
+    category: 'technology',
+    posterLogo: 'Images/Dev.png'
+  },
+  {
+    poster: adminName,
+    articleName: 'Python lesson 1 (Basics)',
+    date: '6 / 10 / 2023 ',
+    article: 'blog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articles',
+    shortArticle: 'blog specialist on technology and programming articles',
+    learnMoreLink: 'pages/html/lesson2',
+    category: 'programming',
+    posterLogo: 'Images/Dev.png'
+  },
+]
